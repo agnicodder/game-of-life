0dc5f0b29a5de2326f01e384773d451003d82279
agnicodder <161954184+agnicodder@users.noreply.github.com>
2025-06-10 12:38:54 +0530

    deleting the comment

--- gameoflife-web/src/main/java/com/wakaleo/gameoflife/webtests/controllers/GameController.java
+++ gameoflife-web/src/main/java/com/wakaleo/gameoflife/webtests/controllers/GameController.java
@@ -17,7 +17,6 @@ import com.wakaleo.gameoflife.domain.Universe;
  *
  * GameController.java
  * Controller to navigate the buttons on the game pages
- * just for testing in jenkins
  */
 @Controller
 @RequestMapping("/game")
